80661754994a4f96b2ab22b77f11b666c365616d
bruna suzuki <brunasuzuki12@gmail.com>
2022-05-23 09:35:26 -0700

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
-# config/routes.rb
 Rails.application.routes.draw do
-  root                   to: 'pages#home'
-  get 'about',           to: 'pages#about',      as: :about
-  get 'contact',         to: 'pages#contact',    as: :contact
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
